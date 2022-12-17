699afde7608f3a5c60c1dbee2b740e94d27eafae
redbluefish <martin.knebel.e@gmail.com>
2022-12-17 15:14:19 +0100

    started work and did assignment 1

--- assignment_3.py
+++ assignment_3.py
@@ -1,5 +1,6 @@
 """
-Intresting link: https://machinelearninggeek.com/solving-transportation-problem-using-linear-programming-in-python/#Transportation_Problem
+Interesting link: https://machinelearninggeek.com/solving-transportation-problem-using-linear-programming-in-python/#Transportation_Problem
+More interesting: https://personal.utdallas.edu/~scniu/OPRE-6201/documents/TP1-Formulation.pdf
 We want to find the optimal way (cost-wise) to transport goods from some suppliers to some demanders. A transportation
 from some factory (supply) to some ware house (demand) is associated with some cost c_ij. A supplier i has supply S_i
 and a warehouse has demand D_j. x_ij denotes the quantity transported. 
@@ -21,6 +22,6 @@ Now we want to solve an actual problem. We have three factories and four warehou
 and transportation cost in relation to another entity. Thus, i = 1,2,3 ; j = 1,2,3,4. Then our problem is
 
 minimize 10*x_11 + 0*x_12 + 20*x_13 + 11*x_14 + 12*x_21 + 7*x_22 + 9*x_23 + 20*x_24 + 0*x_31 + 14*x_32 + 16*x_33 + 18*x_34
-Subject to:
+Subject to: 
 	
 """
